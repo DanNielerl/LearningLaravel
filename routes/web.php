0cb49f402fbfd8336256601d3315a3901ce76f50
DanielRios <daniel.ids.1.3@gmail.com>
2022-10-10 17:49:18 -0400

    Se hizo el avance de /routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cursos', function(){
+    return "Bienvenidos a la pagina cursos";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+Route::get('cursos/{curso}', function($curso){
+    return "Bienvenido al curso: $curso";
+});
+
+Route::get('cursos/{curso}/{categoria?}', function($curso, $categoria = null){
+    if($categoria){
+        return "Bienvenido al curso $curso, de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso $curso";
+    }
+});
+
