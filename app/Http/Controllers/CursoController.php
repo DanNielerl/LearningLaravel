8c1f53450095a904a357f1494ad8e91a121dffb6
DanielRios <daniel.ids.1.3@gmail.com>
2022-10-13 23:10:23 -0400

    Se crearon las vistaws en la carpeta /resources/views/ las cuales sirven para entregar paginas html en la respuesta a las actividades en el navegador

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,14 +8,15 @@ class CursoController extends Controller
 {
     //
     public function index(){
-        return "Bienvenidos a la pagina cursos";
+        return view('cursos.index');
     }
 
     public function create(){
-        return "En esta pagina podras crear un curso";
+        return view('cursos.create');
     }
 
     public function show($curso){
-        return "Bienvenido al curso: $curso";
+        //return view('cursos.show', ['curso' => $curso]); /*tiene otra manera de hacer*/
+        return view('cursos.show',compact('curso'));
     }
 }
