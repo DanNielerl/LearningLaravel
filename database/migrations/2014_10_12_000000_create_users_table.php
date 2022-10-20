ec4a6c3d9782fc81c088dc3664acfbdf4599063b
DanielRios <daniel.ids.1.3@gmail.com>
2022-10-20 10:34:35 -0400

    En el tema de migraciones, se apredio a crear migraciones, a manejar el control de versiones de las base de datos que se crean, eliminan o actualizan en las migraciones, los comandos usados son migrate, make:migrate, migrate:rollback, mirgate:fresh(elimina los registros). Tambien se reviso la estructura de una clase migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id(); //Integer Unsigmed Increment
+            $table->string('name'); //varchar
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //create_at update_at
         });
     }
 
